83b3d9107837b0d48b04aad11214693e50283b17
MyDevGarden <nilima.raundal@gmail.com>
2023-07-13 12:50:51 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<DataContext>(opt =>
@@ -13,12 +14,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("Https://localhost:4200"));
 
 app.UseAuthorization();
 
